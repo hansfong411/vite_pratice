acce083e74766e3aa461abd0e0cc6c4bd7f00c7c
Hans Chang <Hans_Chang@amat.com>
2021-07-26 15:49:32 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+        'plugin:vue/vue3-essential',
+        'prettier',
+    ],
+
+    rules:{
+        'vue/no-used-vars':'error',
+    },
+}
---
+++ src/store/modules/user.js
@@ -0,0 +1,18 @@
+export default{
+
+    state: ()=>({
+
+        name: 'Hans'
+    }),
+    getters : {},
+    mutations:{
+        SET_NAME(state, payload){
+            state.name = payload
+        }
+    },
+    actions:{
+        saveName({commit}, data){
+            commit('SET_NAME', data)
+        },
+    },
+}
